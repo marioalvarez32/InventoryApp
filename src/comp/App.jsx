26894d675cbf404a266fb475137cea844871f482
Mario Alvarez <betoalvarez32@outlook.com>
2020-07-22 18:08:55 -0500

    Boilerplate for application.

---
+++ src/comp/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            value: ""
+        };
+
+
+    }
+
+    render() {
+        return (
+            <div className="home">
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
