145a388653a87eb450236078ce97641c11c0bd6d
Komi Yenkey <yenkeyke25@uww.edu>
2020-07-22 19:22:13 -0500

    Message

--- src/comp/App.jsx
+++ src/comp/App.jsx
@@ -2,21 +2,21 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class App extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            value: "Luigi",
-        };
-    }
+    this.state = {
+      value: "Luigi and Komi",
+    };
+  }
 
-    render() {
-        return (
-            <div className="home">
-                <h1>Hello World</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="home">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
