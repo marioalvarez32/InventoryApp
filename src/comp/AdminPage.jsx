16635f631975d7c18f6cad5d219cbdb46075e43a
Komi Yenkey <yenkeyke25@uww.edu>
2020-08-12 16:53:14 -0500

    Starting working on Admin Page

---
+++ src/comp/AdminPage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class AdminPage extends Component {
+  render() {
+    return <div>Welcome to Admin Page</div>;
+  }
+}
+
+export default AdminPage;
